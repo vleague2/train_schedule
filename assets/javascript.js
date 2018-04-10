f0a41508e0b25a2c06eb44c464887048c436810a
unknown <vleague2@gmail.com>
2018-04-10 19:04:11 -0400

    started javascript psuedocode

---
+++ assets/javascript.js
@@ -0,0 +1,18 @@
+// initialize firebase
+
+// set up firebase database variable
+
+// on submit click
+
+    // pull form values & store in local variables
+
+    // create local variables for next train and minutes till arrival using moment
+
+    // add values to database
+
+// on database update (child added), pass in data snapshot
+
+    // pull values from data snapshat and store in local variables
+
+    // push variable values to html
+
